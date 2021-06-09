d110ed1dc9558feebb9b46b130a66e999118e5f6
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-09 23:42:59 +0300

    Day-11 / Day-12

--- camp-project/src/layouts/CartSummary.jsx
+++ camp-project/src/layouts/CartSummary.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
+import { Link, NavLink } from 'react-router-dom'
+
 
 export default function CartSummary() {
     return (
         <div>
-            <Dropdown item text='Language'>
+            <Dropdown item text='Sepetiniz'>
                 <Dropdown.Menu>
-                    <Dropdown.Item>English</Dropdown.Item>
-                    <Dropdown.Item>Russian</Dropdown.Item>
-                    <Dropdown.Item>Spanish</Dropdown.Item>
+                    <Dropdown.Item>Acer Notebook</Dropdown.Item>
+                    <Dropdown.Item>Asus Notebook</Dropdown.Item>
+                    <Dropdown.Item>Dell Notebook</Dropdown.Item>
+                    <Dropdown.Divider/>
+                    <Dropdown.Item as = {NavLink} to = "/cart">Sepete Git</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
         </div>
