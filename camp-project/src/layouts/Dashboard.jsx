7250d9fe793d3220d2061b52b33a02ab567f5d50
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-09 23:43:20 +0300

    Day-11 / Day-12

--- camp-project/src/layouts/Dashboard.jsx
+++ camp-project/src/layouts/Dashboard.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Categories from "./Categories";
 import ProductList from "../pages/ProductList";
 import { Grid, GridRow } from 'semantic-ui-react'
+import { Route } from 'react-router-dom'
+import ProductDetail from '../pages/ProductDetail';
+import CartDetail from '../pages/CartDetail';
+
 
 export default function Dashboard() {
     return (
@@ -14,7 +18,11 @@ export default function Dashboard() {
                     </Grid.Column>
 
                     <Grid.Column width={12}>
-                        <ProductList />
+                        <Route exact path = "/" component = {ProductList}/>
+                        <Route exact path = "/products" component = {ProductList}/>
+                        <Route path = "/products/details" component = {ProductDetail}/>
+                        <Route path = "/cart" component = {CartDetail}/>
+
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
