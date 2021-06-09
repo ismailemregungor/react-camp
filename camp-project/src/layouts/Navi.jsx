3343a983e97453e32b63b3920db8d7a308617cb9
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-09 23:43:37 +0300

    Day-11 / Day-12

--- camp-project/src/layouts/Navi.jsx
+++ camp-project/src/layouts/Navi.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CartSummary from "./CartSummary"
 import { Button, Container, Menu } from 'semantic-ui-react'
+import SignedOut from './SignedOut'
+import SignedIn from './SignedIn'
 
 export default function Navi() {
+    const [isAuthenticated, setIsAuthenticated] = useState(true)
+
+    //Çıkış Yap Butonu
+    function handleSignOut(params) {
+        setIsAuthenticated(false)
+    }
+
+    function handleSignIn(params) {
+        setIsAuthenticated(true)
+    }
+
     return (
         <div>
-            <Menu inverted fixed = "top">
+            <Menu inverted fixed="top">
                 <Container>
                     <Menu.Item
                         name='home'
@@ -14,18 +27,10 @@ export default function Navi() {
                         name='messages'
                     />
                     <Menu.Menu position='right'>
-
-                        <CartSummary/>
-
-                        
-                        
-
-                        <Menu.Item>
-                            <Button primary>Sign Up</Button>
-                        </Menu.Item>
+                        <CartSummary />
+                        {isAuthenticated ? <SignedIn SignOut={handleSignOut} /> : <SignedOut SignIn={handleSignIn} />}
                     </Menu.Menu>
                 </Container>
-
             </Menu>
         </div>
     )
