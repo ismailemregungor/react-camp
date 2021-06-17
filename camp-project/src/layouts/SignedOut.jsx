3badaefeae9b82c2e9bb5e14b3a6a6343a873e3d
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-17 19:40:34 +0300

    Day-11 / Day-12 / Day-13

--- camp-project/src/layouts/SignedOut.jsx
+++ camp-project/src/layouts/SignedOut.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Button, Menu } from 'semantic-ui-react'
 
-export default function SignedOut({SignIn}) {
+export default function SignedOut({ signIn }) {
     return (
         <div>
             <Menu.Item>
-                <Button primary onClick={SignIn}>Giriş Yap</Button>
-                <Button primary style={{ marginLeft: "0.5em" }}>Kayıt Ol</Button>
+                <Button onClick={signIn} primary>Giriş yap</Button>
+                <Button primary style={{ marginLeft: '0.5em' }}>Kayıt Ol</Button>
             </Menu.Item>
-        </div >
+
+        </div>
     )
 }
