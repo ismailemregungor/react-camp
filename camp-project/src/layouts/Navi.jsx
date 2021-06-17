bfd5f4bef05a7c4a9d5ada4208d2f25ca46b1ed8
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-17 19:41:09 +0300

    Day-11 / Day-12 / Day-13

--- camp-project/src/layouts/Navi.jsx
+++ camp-project/src/layouts/Navi.jsx
@@ -1,37 +1,38 @@
-import React, { useState } from 'react'
-import CartSummary from "./CartSummary"
-import { Button, Container, Menu } from 'semantic-ui-react'
-import SignedOut from './SignedOut'
-import SignedIn from './SignedIn'
+import React, { useState } from "react";
+import CartSummary from "./CartSummary";
+import { Container, Menu } from "semantic-ui-react";
+import SignedOut from "./SignedOut";
+import SignedIn from "./SignedIn";
+import { useHistory } from "react-router";
+import { useSelector } from 'react-redux';
 
 export default function Navi() {
-    const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const { cartItems } = useSelector(state => state.cart)
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const history = useHistory()
+  function handleSignOut() {
+    setIsAuthenticated(false)
+    history.push("/")
+  }
 
-    //Çıkış Yap Butonu
-    function handleSignOut(params) {
-        setIsAuthenticated(false)
-    }
+  function handleSignIn() {
+    setIsAuthenticated(true)
+  }
 
-    function handleSignIn(params) {
-        setIsAuthenticated(true)
-    }
+  return (
+    <div>
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
 
-    return (
-        <div>
-            <Menu inverted fixed="top">
-                <Container>
-                    <Menu.Item
-                        name='home'
-                    />
-                    <Menu.Item
-                        name='messages'
-                    />
-                    <Menu.Menu position='right'>
-                        <CartSummary />
-                        {isAuthenticated ? <SignedIn SignOut={handleSignOut} /> : <SignedOut SignIn={handleSignIn} />}
-                    </Menu.Menu>
-                </Container>
-            </Menu>
-        </div>
-    )
+          <Menu.Menu position="right">
+            {cartItems.length > 0 && <CartSummary />}
+            {isAuthenticated ? <SignedIn signOut={handleSignOut} bisey="1" />
+              : <SignedOut signIn={handleSignIn} />}
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
 }
