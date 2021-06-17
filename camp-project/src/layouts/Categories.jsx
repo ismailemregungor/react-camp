6a6fac671f8ffd0db7e2a6c221dffe8b695f8fe2
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-17 19:41:26 +0300

    Day-11 / Day-12 / Day-13

--- camp-project/src/layouts/Categories.jsx
+++ camp-project/src/layouts/Categories.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 import { Menu } from 'semantic-ui-react'
 
 export default function Categories() {
-    return (
-        <div>
-            <Menu pointing vertical>
-                <Menu.Item
-                    name='home'
-                />
-                <Menu.Item
-                    name='messages'
-                />
-                <Menu.Item
-                    name='friends'
-                />
-            </Menu>
-        </div>
-    )
+  return (
+    <div>
+      <Menu pointing vertical>
+        <Menu.Item
+          name="home"
+        />
+        <Menu.Item
+          name="messages"
+        />
+        <Menu.Item
+          name="friends"
+        />
+      </Menu>
+    </div>
+  );
 }
