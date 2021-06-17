845fe91eef0d223563c11fe89dd7bdad230392b4
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-17 19:46:15 +0300

    Day-13

---
+++ camp-project/src/store/rootReducer.js
@@ -0,0 +1,12 @@
+/**
+ *  Tüm state'ler buraya toplanır
+ */
+import { combineReducers } from "redux";
+import cartReducer from "./reducers/cartReducer";
+
+const rootReducer = combineReducers({
+
+    cart: cartReducer
+})
+
+export default rootReducer;
