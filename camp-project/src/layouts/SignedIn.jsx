d29723f5b20b8dea565a4b383a480db9048a57ad
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-17 19:40:55 +0300

    Day-11 / Day-12 / Day-13

--- camp-project/src/layouts/SignedIn.jsx
+++ camp-project/src/layouts/SignedIn.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Dropdown, Menu, Image } from 'semantic-ui-react'
 
-export default function SignedIn({SignOut}) {
+export default function SignedIn({ signOut }) {
     return (
         <div>
             <Menu.Item>
                 <Image avatar spaced="right" src="https://avatars.githubusercontent.com/u/54360620?v=4" />
-                <Dropdown pointing="top left" text="IEG">
+                <Dropdown pointing="top left" text="Engin">
                     <Dropdown.Menu>
                         <Dropdown.Item text="Bilgilerim" icon="info" />
-                        <Dropdown.Item onClick={SignOut} text="Çıkış Yap" icon="sign-out" />
+                        <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out" />
                     </Dropdown.Menu>
                 </Dropdown>
             </Menu.Item>
         </div>
     )
 }
+
+//22.50 Dersteyiz
