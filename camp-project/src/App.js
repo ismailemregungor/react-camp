f2e1bbb4b286862613b3729d66a91b7baaa06845
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-17 19:44:45 +0300

    Day-11 / Day-13

--- camp-project/src/App.js
+++ camp-project/src/App.js
@@ -2,18 +2,16 @@ import './App.css';
 import Dashboard from './layouts/Dashboard';
 import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react';
-import Navi from "./layouts/Navi";
+import Navi from './layouts/Navi';
 
 
 function App() {
   return (
     <div className="App">
-      <Navi/>
+      <Navi />
       <Container className="main">
         <Dashboard />
       </Container>
-
-
     </div>
   );
 }
--- camp-project/src/index.js
+++ camp-project/src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from "./store/configureStore";
+import "react-toastify/dist/ReactToastify.min.css"
 
+const store = configureStore()
 ReactDOM.render(
-//<React.StrictMode>
-    <App />
-  //</React.StrictMode>,
-  ,document.getElementById('root')
-);
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
