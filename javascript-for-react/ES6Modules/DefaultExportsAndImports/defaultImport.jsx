f3887c6e4deb44b153e89e81ba501e0cc74658fa
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-04-10 16:05:09 +0300

    Create defaultImport.jsx

---
+++ javascript-for-react/ES6Modules/DefaultExportsAndImports/defaultImport.jsx
@@ -0,0 +1,4 @@
+import sayHello from './defaultExports';
+import { default as sayHello, name, user } from './defaultExports';
+// Immediate re-export (anında yeniden dışa aktarma)
+export * from './defaultExports';
