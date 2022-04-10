868bc514da480c9668ad20166138abd9c03d1b5c
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-04-10 16:06:22 +0300

    Create namedExports.jsx

---
+++ javascript-for-react/ES6Modules/NamedExportsAndImports/namedExports.jsx
@@ -0,0 +1,12 @@
+/**
+ * Named Exports & Imports (isimli dışa/içe aktarmalar)
+ */
+
+export const name = 'ieg';
+
+export const user = { name, age: 23 };
+
+export function sayHello(name) {
+  return `Merhaba $(name)`;
+}
+//export { name, user, sayHello };
