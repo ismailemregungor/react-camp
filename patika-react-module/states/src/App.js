2c8bab55ebcaad147bc4286de03967859d9ae7fd
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-07-21 00:51:54 +0300

    states

--- patika-react-module/states/src/App.js
+++ patika-react-module/states/src/App.js
@@ -1,8 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+	const [name, setName] = useState("Ömer");
+	const [age, setAge] = useState(29);
+	const [friends, setFriends] = useState(["İsmail", "Emre"]);
+	const [address, setAddress] = useState({ title: "Istanbul", zip: 34756 });
+
+	return (
+		<div className="App">
+			<h1>Merhaba {name}!</h1>
+			<h2>{age}</h2>
+
+			<button onClick={() => setName("İsmail")}>Change name</button>
+			<button onClick={() => setAge(23)}>Change age</button>
+
+			<hr />
+			<br></br>
+
+			<h2>Friends</h2>
+			{friends.map((friend, index) => (
+				<div key={index}>{friend}</div>
+			))}
+
+			<br />
+
+			<button onClick={() => setFriends([...friends, "Sude"])}>
+				add new friend
+			</button>
+
+			<hr />
+			<br></br>
+
+			<h2>Address</h2>
+			<div>
+				{address.title} {address.zip}
+			</div>
+
+			<br />
+
+			<button
+				onClick={() => setAddress({ ...address, zip: 44444, title: "Ankara" })}
+			>
+				change the address
+			</button>
+		</div>
+	);
 }
 
 export default App;
