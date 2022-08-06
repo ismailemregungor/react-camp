4f6c0b9c836cf0f150ce355bd0f13bb14feff307
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-08-06 14:31:52 +0300

    delete directory

--- kodlamaio-react-camp/camp-project/src/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
-import rootReducer from "./rootReducer";
-
-export function configureStore() {
-
-    return createStore(rootReducer, devToolsEnhancer());
-
-}
